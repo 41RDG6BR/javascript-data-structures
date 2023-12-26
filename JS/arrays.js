08a4bcb25e6dafb856eadf74fd87d8f83482b087
Rodrigo Nogueira <rdg6design@gmail.com>
2023-12-26 11:41:36 -0300

    :sparkles: Feat: generate and print the first 20 numbers of the Fibonacci sequence
    This commit introduces a code snippet that calculates and prints the first 20 numbers of the Fibonacci sequence.

--- JS/arrays.js
+++ JS/arrays.js
@@ -13,3 +13,22 @@ const numbers = [0, 1, 2, 3, 4, 5];
 let daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
 console.log(daysOfWeek);
+
+// Discover the first 20 numbers from the Fibonacci sequence
+
+/**
+ * Represents an array to store Fibonacci numbers.
+ * @type {number[]}
+ */
+const fibonacci = [0, 1];
+
+for (let i = 2; i < 20; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+/**
+ * Print the Fibonacci sequence.
+ */
+for (let i = 1; i < fibonacci.length; i++) {
+    console.log(fibonacci[i]);
+}
