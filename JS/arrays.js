9397f12a714235614a545f293ec8f132b76ec529
Rodrigo Nogueira <rdg6design@gmail.com>
2023-12-26 11:36:50 -0300

    :memo: Docs: add documentation for daysOfWeek array
    Changes:

--- JS/arrays.js
+++ JS/arrays.js
@@ -5,3 +5,11 @@
  * @type {number[]}
  */
 const numbers = [0, 1, 2, 3, 4, 5];
+
+/**
+ * Represents the days of the week.
+ * @type {string[]}
+ */
+let daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+console.log(daysOfWeek);
