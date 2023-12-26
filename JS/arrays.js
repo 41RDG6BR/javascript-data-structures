e0bdc448680265f173834150ae6ad7e353b3b9af
Rodrigo Nogueira <rdg6design@gmail.com>
2023-12-26 19:54:12 -0300

    :sparkles: Feat: add examples for find and findIndex methods
    ECMAScript 2015

--- JS/arrays.js
+++ JS/arrays.js
@@ -470,3 +470,14 @@ console.log(numbers.indexOf(100));
 numbers.push(10);
 console.log(numbers.lastIndexOf(10));
 console.log(numbers.lastIndexOf(100));
+
+// ECMAScript2015 - The methods find and findIndex
+
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+function multipleOf13(element, index, array) {
+    return (element % 13 == 0);
+}
+
+console.log(numbers.find(multipleOf13));
+console.log(numbers.findIndex(multipleOf13));
