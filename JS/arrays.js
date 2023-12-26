aa10a8605f18b97c0617999232167f0f5fcdab15
Rodrigo Nogueira <rdg6design@gmail.com>
2023-12-26 15:37:20 -0300

    :art: Chore: implement custom ordering function
    Introduce a custom ordering function, comparePerson, for sorting the 'friends' array based on the 'age' property. It also includes an ES2017 style comma at the end of the array.

--- JS/arrays.js
+++ JS/arrays.js
@@ -419,3 +419,23 @@ function compare(a, b) {
 }
 
 numbers.sort(compare);
+
+// Custom ordering
+
+const friends = [
+    {name: 'John', age: 30},
+    {name: 'Ana', age: 20},
+    {name: 'Chris', age: 25}, // comma at the end: ES2017
+];
+
+function comparePerson(a, b) {
+    if(a.age < b.age) {
+        return -1;
+    }
+    if(a.age > b.age) {
+        return 1;
+    }
+    return 0;
+}
+
+console.log(friends.sort(comparePerson));
