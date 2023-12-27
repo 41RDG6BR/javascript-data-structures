3bda6894e53e278bcb611efb33895abe39cb143f
Rodrigo Nogueira <rdg6design@gmail.com>
2023-12-27 06:11:09 -0300

    chore: adds a constructor to the Stack class

--- JS/stack.js
+++ JS/stack.js
@@ -1,3 +1,7 @@
 // Class definition for a Stack
 class Stack {
-}
+    // Constructor initializes an empty array and sets the count to 0
+    constructor() {
+        this.items = [];
+        this.count = 0;
+    }
