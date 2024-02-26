817c1fb24bc47edb875ebafa35189d73f40e7f0e
Rodrigo Nogueira <rdg6design@gmail.com>
2024-02-26 13:46:33 -0300

    logs para cada iteracao do algoritmo

--- JS/algoritmos/bucketSort.js
+++ JS/algoritmos/bucketSort.js
@@ -1,56 +1,66 @@
 import { insertionSort } from './insertionSort.js';
 
-function bucketSort(array, bucketSize = 5){
-    if(array.length < 2) {
+function bucketSort(array, bucketSize = 5) {
+    if (array.length < 2) {
         return array;
     }
 
+    console.log('Array Inicial:', array);
+
     const buckets = createBuckets(array, bucketSize);
-    return sortBuckets(buckets); 
+    console.log('Baldes Iniciais:', buckets);
+
+    const sortedArray = sortBuckets(buckets);
+    console.log('Array Final Ordenado:', sortedArray);
+
+    return sortedArray;
 }
 
 function createBuckets(array, bucketSize) {
     let minValue = array[0];
     let maxValue = array[0];
 
-    for(let i = 1; i < array.length; i++) {
-        if(array[i] < minValue) {
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minValue) {
             minValue = array[i];
         } else if (array[i] > maxValue) {
             maxValue = array[i];
-
         }
     }
 
-    const bucketCount =  Math.floor((maxValue - minValue) / bucketSize) + 1;
+    const bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
     const buckets = [];
+
     for (let i = 0; i < bucketCount; i++) {
         buckets[i] = [];
     }
-    for(let i = 0; i < array.length; i++) {
+
+    for (let i = 0; i < array.length; i++) {
         const bucketIndex = Math.floor((array[i] - minValue) / bucketSize);
         buckets[bucketIndex].push(array[i]);
     }
-    return buckets;
 
+    console.log('Baldes Criados:', buckets);
+
+    return buckets;
 }
 
 function sortBuckets(buckets) {
     const sortedArray = [];
 
-    for(let i = 0; i < buckets.length; i++) {
-        if(buckets[i] != null) {
+    for (let i = 0; i < buckets.length; i++) {
+        if (buckets[i] != null) {
+            console.log(`Ordenando Balde ${i + 1}:`, buckets[i]);
             insertionSort(buckets[i]);
+            console.log(`Balde ${i + 1} Ordenado:`, buckets[i]);
             sortedArray.push(...buckets[i]);
+            console.log('Array Parcial Ordenado:', sortedArray);
         }
     }
+
     return sortedArray;
 }
 
-
 // Exemplo de uso:
 const arrayDesordenado = [3, 5, 1, 6, 4, 7, 2];
-console.log('Array Original:', arrayDesordenado);
-
-const arrayOrdenado = bucketSort(arrayDesordenado);
-console.log('Array Final Ordenado:', arrayOrdenado);
+bucketSort(arrayDesordenado);
