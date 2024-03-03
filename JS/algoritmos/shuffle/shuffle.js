0be17a1c611b70cc7c73c81e5f11d5ca573c8d50
Rodrigo Nogueira <rdg6design@gmail.com>
2024-03-03 08:29:55 -0300

    export quickSort

---
+++ JS/algoritmos/shuffle/shuffle.js
@@ -0,0 +1,15 @@
+import { swap } from '../../../util.js';
+
+function shuffle(array) {
+    for(let i = array.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        swap(array, i, randomIndex);
+    }
+    return array;
+}
+
+const array = [0,1,2,3,4,5];
+
+const result = shuffle(array, 3);
+
+console.log(result);
